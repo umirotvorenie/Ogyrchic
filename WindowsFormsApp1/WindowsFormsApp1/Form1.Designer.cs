ccb02833ba7ab1176c05c40943249a6caf3199ef
Kate <138885230+umirotvorenie@users.noreply.github.com>
2024-10-30 01:51:44 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class AIDA8
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.NameObject = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(41, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(165, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Вывести";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(319, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.PeachPuff;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Процессор",
+            "Материнская плата",
+            "Оперативная память",
+            "Накопитель",
+            "Видеокарта",
+            "Биос"});
+            this.comboBox1.Location = new System.Drawing.Point(204, 53);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(219, 33);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(41, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(41, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.Location = new System.Drawing.Point(41, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label5.Location = new System.Drawing.Point(41, 399);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightCoral;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button3.FlatAppearance.BorderSize = 2;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button3.Location = new System.Drawing.Point(486, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 47);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Выйти";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // NameObject
+            // 
+            this.NameObject.AutoSize = true;
+            this.NameObject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameObject.Location = new System.Drawing.Point(41, 183);
+            this.NameObject.Name = "NameObject";
+            this.NameObject.Size = new System.Drawing.Size(107, 25);
+            this.NameObject.TabIndex = 9;
+            this.NameObject.Text = "Название";
+            this.NameObject.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(136, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(356, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Выберите нужное коплектующее ПК";
+            // 
+            // AIDA8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(635, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.NameObject);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "AIDA8";
+            this.Text = "AIDA8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label NameObject;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class AIDA8 : Form
+    {
+        public string zhelezo;
+
+        public AIDA8()
+        {
+            InitializeComponent();
+            Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (var config in new ManagementObjectSearcher($"select * from {zhelezo}").Get())
+                {
+                    label1.Text = "Название: " + (string)config["Name"];
+
+                    if (comboBox1.Text == "Процессор")
+                    {
+                        NameObject.Text = "Характеристики процессора:";
+                        label2.Text = "Производитель: " + config["Manufacturer"];
+                        label3.Text = "Количество ядер: " + (UInt32)config["NumberOfCores"];
+                        label4.Text = "Количество потоков: " + (UInt32)config["ThreadCount"];
+
+                    }
+                    else if (comboBox1.Text == "Материнская плата")
+                    {
+                        NameObject.Text = "Характеристики материнской платы: ";
+                        label2.Text = "Тип системной шины: " + config["PrimaryBusType"];
+                        label3.Text = "Тип вторичной шины: " + config["SecondaryBusType"];
+                        label4.Text = "Имя системы: " + config["SystemName"];
+
+                    }
+                    else if (comboBox1.Text == "Оперативная память")
+                    {
+                        NameObject.Text = "Характеристики оперативной памяти: ";
+                        label2.Text = "Номер слота: " + config["BankLabel"];
+                        label3.Text = "Частота: " + config["ConfiguredClockSpeed"];
+                        label4.Text = "Вольты: " + config["ConfiguredVoltage"];
+                        label5.Text = "Производитель: " + config["Manufacturer"];
+                    }
+                    else if (comboBox1.Text == "Накопитель")
+                    {
+                        NameObject.Text = "Характеристики накопителя: ";
+                        label2.Text = "Модель: " + config["Model"];
+                        label3.Text = "Тип интерфейса: " + config["InterfaceType"];
+                        label4.Text = "Размер сектора: " + config["BytesPerSector"];
+                    }
+                    else if (comboBox1.Text == "Видеокарта")
+                    {
+                        NameObject.Text = "Характеристики видеокарты: ";
+                        label2.Text = "Производитель: " + config["AdapterCompatibility"];
+                        label3.Text = "Тип DAC: " + config["AdapterDACType"];
+                        label4.Text = "Версия драйвера: " + config["DriverVersion"];
+                    }
+                    else if (comboBox1.Text == "Биос")
+                    {
+                        NameObject.Text = "Характеристики биоса: ";
+                        label2.Text = "Версия биоса: " + config["BIOSVersion"];
+                        label3.Text = "Производитель: " + config["Manufacturer"];
+                        label4.Text = "Дата выхода: " + config["ReleaseDate"];
+                    }
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Комплектующее не было найдено");
+            }
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string coll = comboBox1.Text;
+            switch (coll)
+            {
+                case "Процессор":
+                    zhelezo = "Win32_Processor";
+                    break;
+
+                case "Материнская плата":
+                    zhelezo = "Win32_MotherboardDevice";
+                    break;
+                case "Оперативная память":
+                    zhelezo = "Win32_PhysicalMemory";
+                    break;
+                case "Накопитель":
+                    zhelezo = "Win32_DiskDrive";
+                    break;
+                case "Видеокарта":
+                    zhelezo = "Win32_VideoController";
+                    break;
+                case "Биос":
+                    zhelezo = "Win32_BIOS";
+                    break;
+            }
+            Clear();
+
+        }
+
+        public void Clear()
+        {
+            label1.Text = " ";
+            label2.Text = " ";
+            label3.Text = " ";
+            label4.Text = " ";
+            label5.Text = " ";
+            NameObject.Text = " ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
